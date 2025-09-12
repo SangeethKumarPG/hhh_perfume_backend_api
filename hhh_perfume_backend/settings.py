4565eb357a60fca2edbca1d7e19a47691b30ed53
Athira2354 <athirapradeeep.cp@gmail.com>
2025-09-12 16:17:57 +0530

    payment section

--- hhh_perfume_backend/settings.py
+++ hhh_perfume_backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hhh_perfume_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'payment','template','payment')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
